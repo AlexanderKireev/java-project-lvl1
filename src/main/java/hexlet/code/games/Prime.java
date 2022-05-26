2001b33bd8278e36c1d4155e93d98082d96565f1
Alexander Kireev <alexanderkir1980@gmail.com>
2022-05-27 02:11:59 +0300

    edit Prime.java

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -7,7 +7,7 @@ public class Prime {
     public static void startGame() {
         String question = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
         String[][] questionsAndAnswers = new String[NUMBEROFQUESTIONS][2];
-        final int min = -100; // расширил диапазон до -100
+        final int min = -100;
         final int max = 100;
         for (var qwsAndAns : questionsAndAnswers) {
             int randomNumber = getRand(min, max);
@@ -17,12 +17,10 @@ public class Prime {
         runGame(questionsAndAnswers, question);
     }
     public static String isPrime(int a) {
-        // добавил проверку для чисел < 2
-        if (a % 2 == 0 || a < 2) {
+        if ((a % 2 == 0 || a < 2) && a != 2) {
             return "no";
         }
         final int num3 = 3;
-        // уменьшил число переборов, от тройки до i^2 с шагом 2
         for (var i = num3; i * i <= a; i += 2) {
             if (a % i == 0) {
                 return "no";
